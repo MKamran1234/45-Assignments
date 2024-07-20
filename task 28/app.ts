eda2b798fbcfe3f3f6d2ad0ea3e5728b73131538
MKamran1234 <arfanboys345@gmail.com>
2024-07-20 10:58:59 +0500

    ulpoading 45 assignments

--- task 28/app.ts
+++ task 28/app.ts
@@ -9,12 +9,12 @@ else if(age >= 2 &&  age < 4){
 else if(age >= 4 && age < 13) {
     console.log("The person is a kid.");
 }
-else if (age >=13 && age < 20){
+else if (age >=13 && age < 18){
     console.log("The person is teenager.");
 }
-else if (age >= 20 && age < 65){
+else if (age >= 18 && age < 65){
     console.log("The person is an adult.");
 }
 else{
-    console.log("The person is an rlder.");
+    console.log("The person is an older.");
 }
